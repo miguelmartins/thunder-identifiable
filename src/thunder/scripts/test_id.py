88126aa21fcb33ee0b63793555339d65bc343831
Miguel Martins <miguelmartins@Miguels-MacBook-Pro.local>
2025-09-30 14:41:00 +0200

    Add first loggable identifiable experiment

---
+++ src/thunder/scripts/test_id.py
@@ -0,0 +1,147 @@
+from typing import Tuple
+from lightning.pytorch.loggers import TensorBoardLogger
+import torch
+import lightning as L
+import h5py
+
+from src.thunder.utils.data import load_embeddings
+import torchmetrics
+
+
+class EmbeddingsInstanceDataset(torch.utils.data.Dataset):
+    def __init__(self, embeddings) -> None:
+        self.embeddings = embeddings
+
+    def __getitem__(self, index) -> Tuple[torch.Tensor, int]:
+        return self.embeddings[index], index
+
+    def __len__(self) -> int:
+        return len(self.embeddings)
+
+
+class InstanceLearner(L.LightningModule):
+    def __init__(
+        self,
+        linear_projector: torch.nn.Linear,
+        learning_rate: float,
+        metrics: torchmetrics.MetricCollection,
+    ) -> None:
+        super().__init__()
+        self.linear_projector = linear_projector
+        self.learning_rate = learning_rate
+        self.automatic_optimization = False
+        self.train_metrics = metrics
+        self.validation_metrics = self.train_metrics.clone(prefix="val_")
+        print(f"num classes {linear_projector.weight.data.shape[0]}")
+
+    def forward(self, batch):
+        x, _ = batch
+        return self.linear_projector(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.linear_projector(x)
+        loss = torch.nn.functional.cross_entropy(y_hat, y)
+        opt = self.optimizers()
+        opt.zero_grad()
+        self.manual_backward(loss)
+        opt.step()
+        metric_values = self.train_metrics(y_hat, y)
+        # Logging to TensorBoard (if installed) by default
+        self.log("train_loss", loss, prog_bar=True)
+        self.log_dict(metric_values, prog_bar=True)
+        return loss
+
+    def on_train_epoch_end(self):
+        schedulers = self.lr_schedulers()
+        try:
+            for scheduler in schedulers:
+                scheduler.step()
+        except TypeError:
+            schedulers.step()
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.linear_projector(x)
+        # - Input: Shape `(C)`, `(N, C)` or `(N, C, d_1, d_2, ..., d_K)` with `K \geq 1`
+        loss = torch.nn.functional.cross_entropy(y_hat, y)
+        metric_values = self.validation_metrics(y_hat, y)
+        # Logging to TensorBoard (if installed) by default
+        self.log("val_loss", loss, prog_bar=True)
+        self.log_dict(metric_values, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=LR)
+        warmup = torch.optim.lr_scheduler.LinearLR(
+            optimizer=optimizer, start_factor=1e-8, end_factor=1, total_iters=WARMUP
+        )
+        cosine_ann = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=MAX_EPOCHS
+        )
+
+        return [optimizer], [warmup, cosine_ann]  # [warmup, cosine_ann]
+
+
+N = 1000
+MAX_EPOCHS = 150
+WEIGHT_DECAY = 0.05
+LR = 1e-4
+WARMUP = int(0.1 * MAX_EPOCHS)
+
+
+def main():
+    folder = "/Users/miguelmartins/Projects/thunder-identifiable/src/thunder/embeddings/conch/"
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu",
+    )[0]
+    emb, labels = load_embeddings(embeddings_folder=folder, splits=["train"])
+    for x, y in emb.items():
+        embeddings = torch.from_numpy(y)
+        emb[x] = EmbeddingsInstanceDataset(embeddings)
+        print(type(x), type(y), type(embeddings), type(emb[x]))
+
+    train = emb["train"]
+    perm = torch.randperm(N)  # draw once
+    train_samples = torch.utils.data.Subset(train, perm)
+    assert len(train_samples) == N
+    # TODO: add sampler so that training N=1000
+    train_dl = torch.utils.data.DataLoader(train_samples, batch_size=32)
+
+    linear_map = torch.nn.Linear(
+        in_features=512, out_features=len(train_samples), bias=False
+    )
+    metrics = torchmetrics.MetricCollection(
+        {
+            "id_acc": torchmetrics.Accuracy(
+                task="multiclass",
+                # W projects from d to |I|, so we can use this to get the number of "instances"
+                num_classes=N,
+            )
+        }
+    )
+
+    id_model = InstanceLearner(
+        linear_projector=linear_map, metrics=metrics, learning_rate=LR
+    )
+    logger = TensorBoardLogger("logs", name="conch")
+    trainer = L.Trainer(
+        max_epochs=MAX_EPOCHS,
+        devices=1,
+        accelerator=device,
+        # check_val_every_n_epoch=config.check_val_every_n_epoch,
+        # callbacks=[
+        #     LinearDisentanglement(),
+        # ],
+        logger=logger,
+    )
+
+    trainer.fit(id_model, train_dl)
+
+
+if __name__ == "__main__":
+    main()
